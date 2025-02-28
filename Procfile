c9d8e34b3d7b58702d329db6ca7f223764f8d35f
khanh27202 <khanhlanle27@gmail.com>
2025-02-28 01:19:03 -0500

    procfile change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT index1:app
+web: gunicorn --bind 0.0.0.0:$PORT index1:server-team1snowremoval
--- index1.py
+++ index1.py
@@ -85,4 +85,4 @@ def get_hossain():
 
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
